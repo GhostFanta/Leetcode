fe483052fd0fa3fc95bdb648c3797a4cbc23f4aa
GhostFanta <huiyueguan111@gmail.com>
2018-12-09 13:56:59 -0400

    442 java

---
+++ java/442.find-all-duplicates-in-an-array.java
@@ -0,0 +1,16 @@
+class Solution {
+    public List<Integer> findDuplicates(int[] nums) {
+
+        List<Integer> res = new ArrayList<>();
+
+        for(int num : nums){
+            int index = Math.abs(num) - 1;
+            if(nums[index] < 0){
+                res.add(Math.abs(num));
+            }
+            nums[index] = -nums[index];
+        }
+
+        return res;
+    }
+}
